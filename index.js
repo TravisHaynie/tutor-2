33481f54f99a84c569163fc49581bfdc5812b5ed
unknown <travihay@gmail.com>
2025-03-08 18:20:12 -0600

    changed js

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
-const form = document.getElementById('form');
-const submitBtn = document.getElementById('submit-btn');
-const array = ["name", "email"];
-const renderHtml = document.getElementById('render-html')
+const names = ['travis', 'marcie', 'don'];
 
-function renderFormInputs() {
-    
-    for(value of array) {
-        console.log(value);
-        renderHtml.innerHTML += 
-            `
-                <label for=${value}>${value}</label>
-                <input name=${value} type=${value} id=${value} />
-            `;
+names.includes('travis')
+
+//create a function called includes that takes in the arr and it is going to return a boolean
+
+function includes(arr, str) {
+
+    for (value of arr) {
+      
+        if (value === str) {
+            return true;
+        } else {
+            return false;
+        }
     }
-    
+
+   
+
 }
 
-renderFormInputs();
-
-submitBtn.addEventListener('click', function(e) {
-    e.preventDefault();
-        const formData = new FormData(form);
-        const newFormDataName = formData.get('name');
-        const newFormDataEmail = formData.get('email');
-        console.log(newFormDataName);
-        console.log(newFormDataEmail);
-});
-
+console.log(includes(names, 'peter')); // true
+console.log(includes(names, 'travis')); // false
