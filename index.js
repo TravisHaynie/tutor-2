98b5680d0f57115abbd2648ff0c80564c366a749
unknown <travihay@gmail.com>
2025-03-09 16:26:35 -0500

    changed js

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-const names = ['travis', 'marcie', 'don'];
+const names = ['travis', 'marcie', 'don', 'carl', 'marcella', 'jack', 'tim'];
+const games = ['apex legends', 'raindow six seige', 'LoL', 'Elden Ring'];
+const movies = ['2012', 'avengers', 'black panther', 'IT', 'scream'];
 
-names.includes('travis')
+
 
 //create a function called includes that takes in the arr and it is going to return a boolean
 
 function includes(arr, str) {
-
+ 
     for (value of arr) {
-      
         if (value === str) {
             return true;
-        } else {
-            return false;
-        }
+        } 
     }
-
+    return false;
    
-
 }
 
-console.log(includes(names, 'peter')); // false
 console.log(includes(names, 'travis')); // true
-console.log(includes(names, 'marcie')); // true
-console.log(includes(names, 'mark')) //false
+console.log(includes(names, 'pops')); // false
+console.log(includes(names, 'lark')); // false
+console.log(includes(names, 'marcella')); //true
+console.log(includes(names, 'tim')); //true
+
+console.log(includes(games, 'apex legends')); // true
+console.log(includes(games, 'raindow six seige')); // true
+console.log(includes(games, 'mario cart')); // false
+console.log(includes(games, 'battlefield')); //false
+console.log(includes(games, 'LoL')); //true
+
+console.log(includes(movies, 'cars')); // false
+console.log(includes(movies, 'minons')); // false
+console.log(includes(movies, 'shrek')); // false
+console.log(includes(movies, 'jaws')); //false
+console.log(includes(movies, 'king arthur')); //false
+
+
